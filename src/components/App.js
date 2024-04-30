dc73ca7f63cdd6a27f898741d8dcc8d37bcce90d
Chrispine Oburu <chrizoburu65@gmail.com>
2024-04-30 07:35:30 +0300

    fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
-// create your App component here
+import React, {useEffect, useState } from 'react';
+
+function App() {
+    const [url, setUrl] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(response => response.json())
+            .then((data) => {
+                setUrl(data.message)
+                setIsLoaded(true)
+            });
+
+
+    }, []);
+
+    if (!isLoaded) return <p>Loading...</p>;
+
+    return (
+        <div className="App">
+            <img src={url} alt="A Random Dog" />
+        </div>
+    );
+}
+
+export default App;
